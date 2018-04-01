a27e62595b29846d1c63a1a829bacf428873f8cb
siecola <siecola@gmail.com>
2018-04-01 12:33:47 -0300

    Added security, service and repository layers

---
+++ src/main/java/br/com/siecola/salesprovider/model/OrderItem.java
@@ -0,0 +1,43 @@
+package br.com.siecola.salesprovider.model;
+
+import java.io.Serializable;
+
+public class OrderItem implements Serializable {
+
+    private long id;
+    private long productId;
+    private Product product;
+    private long orderId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
+    }
+}
